825b50d78584f25a12715361602e185c4ad1f690
Haiker <mr.haiker@mail.ru>
2015-11-19 15:44:30 +0200

    fix js

--- js/main.js
+++ js/main.js
@@ -1,44 +1,49 @@
-$('#search').keyup(function () {
-    var textInput = $('#search').val();
-    var tempArray = textInput.split(' ');
+var main = function () {
+    $('#search').keyup(function () {
+        var textInput = $('#search').val();
+        var tempArray = textInput.split(' ');
 
-    // получение данных с сервера
-    $.getJSON('json/players.json', function (data) {
-        $('.results').empty();
-        var allItems = 0; // переменная которая выведет количество найденых совпадений
-        $.each(data, function (key, val) {
-            var strSearchBox = val.position+" "+val.nationality+" "+val.name+" "+val.id;
-            var SearchBox = strSearchBox.split(' ');
-            if(SearchInArray(tempArray,SearchBox)) {
-                allItems += 1;
-                constructorOutput(val.name, val.id, val.position, val.nationalyty, val.marketValue);
+        // получение данных с сервера
+        $.getJSON('json/players.json', function (data) {
+            $('.results').empty();
+            var allItems = 0; // переменная которая выведет количество найденых совпадений
+            $.each(data, function (key, val) {
+                var strSearchBox = val.position + " " + val.nationality + " " + val.name + " " + val.id;
+                var SearchBox = strSearchBox.split(' ');
+                if (SearchInArray(tempArray, SearchBox)) { // сравнение массивов на совпадения
+                    allItems += 1;
+                    constructorOutput(val.name, val.id, val.position, val.nationality, val.marketValue);
+                }
+            });
+
+            // вывод на экран
+            if (allItems === 0 && tempArray != '') {  // если найденых элементов = 0 -> выводим алерт
+                $($('<div class="alert alert-danger" role="alert"><b>Увы</b>, но по Вашему запросу ничего не найдено</div>')).prependTo('.results');
             }
-        });
-        // вывод на экран
-        if (allItems === 0 && tempArray!='') {  // если найденых элементов = 0 -> выводим
-            $($('<div class="alert alert-danger" role="alert"><b>Увы</b>, но по Вашему запросу ничего не найдено</div>')).prependTo('.results');
-        }
 
-        var findItems = ''; // количество найденных элементов
-        if(allItems!=0 && tempArray!=''){ // если в input ничего не указано, количество найденных элементов не будет выводится
-            findItems = 'Найдено ';
-            findItems += allItems;
-            findItems += keyWord(allItems); //получаем слово "элеметнов" видоизменееное под число
-        }
-        $('#find-items').text(findItems);
+            var findItems = ''; // количество найденных элементов
+            if (allItems != 0 && tempArray != '') { // если в input ничего не указано, количество найденных элементов не будет выводится
+                findItems = 'Найдено ';
+                findItems += allItems;
+                findItems += keyWord(allItems); //получаем слово "элеметнов" видоизменееное под число
+            }
+            $('#find-items').text(findItems);
+        });
     });
-});
 
 // анимация со ссылками
-setTimeout(function(){
-    $('#first-article').fadeIn(400);
-}, 100);
-setTimeout(function(){
-    $('#second-article').fadeIn(400);
-}, 300);
-setTimeout(function(){
-    $('#third-article').fadeIn(400);
-}, 500);
-setTimeout(function(){
-    $('#fourth-article').fadeIn(400);
-}, 700);
+    setTimeout(function () {
+        $('#first-article').fadeIn(400);
+    }, 100);
+    setTimeout(function () {
+        $('#second-article').fadeIn(400);
+    }, 300);
+    setTimeout(function () {
+        $('#third-article').fadeIn(400);
+    }, 500);
+    setTimeout(function () {
+        $('#fourth-article').fadeIn(400);
+    }, 700);
+};
+
+$(document).ready(main());
